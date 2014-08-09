7bebbd8828a2646510460f32a34d1041bbf930a8
Mazen K <email@mazenmc.io>
2014-08-09 16:47:34 +0400

    Fixed NPE

--- src/main/java/io/mazenmc/prisonrankup/listeners/JoinListener.java
+++ src/main/java/io/mazenmc/prisonrankup/listeners/JoinListener.java
@@ -43,48 +43,59 @@ public class JoinListener implements Listener {
                         // Now we're going to set his uuid
                         Field field = entityPlayer.getField("uniqueID");
                         field.set(nmsEntity, id);
+
+                        // Continue the running the event
+
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                UUIDManager uuidManager = UUIDManager.getInstance();
+
+                                if(!(uuidManager.contains(event.getPlayer().getName())) &&
+                                        (uuidManager.getName(event.getPlayer().getUniqueId()) != null) ||
+                                        !(uuidManager.getName(event.getPlayer().getUniqueId()).equals(event.getPlayer().getName()))) {
+
+                                    UUIDManager.getInstance().updateName(event.getPlayer().getUniqueId(), event.getPlayer().getName());
+                                }
+
+                                if(!DataManager.getInstance().contains(event.getPlayer().getUniqueId())) {
+                                    DataManager.getInstance().addPlayer(event.getPlayer().getName());
+                                }
+
+                                if(PrisonRankupConfig.CONFIG.getBoolean("Timed Requirement")) {
+                                    long seconds = 0L;
+                                    int time = PrisonRankupConfig.CONFIG.getInt("Time Interval");
+
+                                    switch(PrisonRankupConfig.CONFIG.getString("Time type").toLowerCase().charAt(0)) {
+                                        case 's':
+                                            seconds += time;
+                                            break;
+
+                                        case 'm':
+                                            seconds += time * 60;
+                                            break;
+
+                                        case 'h':
+                                            seconds += time * 360;
+                                            break;
+                                    }
+
+                                    TimeManager.getInstance().addPlayer(DataManager.getInstance().getPlayer(event.getPlayer().getName()), seconds);
+                                }
+
+                                if(UpdaterManager.getInstance().isUpdateAvailable() && event.getPlayer().hasPermission("prisonrankup.update")) {
+                                    UpdaterManager um = UpdaterManager.getInstance();
+
+                                    event.getPlayer().sendMessage(ChatColor.GOLD + "An update is available: " + um.getName() + ", a " +
+                                            um.getType() + " for " + um.getVersion() + " available at " + um.getVersion());
+                                }
+                            }
+                        }.runTask(PrisonRankupPlugin.getInstance());
                     }catch(Exception ex) {
                         ex.printStackTrace();
                     }
                 }
             }.runTaskAsynchronously(PrisonRankupPlugin.getInstance());
-        }
-
-        if(!(UUIDManager.getInstance().contains(event.getPlayer().getName())) ||
-                !(UUIDManager.getInstance().getName(event.getPlayer().getUniqueId()).equals(event.getPlayer().getName()))) {
-            UUIDManager.getInstance().updateName(event.getPlayer().getUniqueId(), event.getPlayer().getName());
-        }
-
-        if(!DataManager.getInstance().contains(event.getPlayer().getUniqueId())) {
-            DataManager.getInstance().addPlayer(event.getPlayer().getName());
-        }
-
-        if(PrisonRankupConfig.CONFIG.getBoolean("Timed Requirement")) {
-            long seconds = 0L;
-            int time = PrisonRankupConfig.CONFIG.getInt("Time Interval");
-
-            switch(PrisonRankupConfig.CONFIG.getString("Time type").toLowerCase().charAt(0)) {
-                case 's':
-                    seconds += time;
-                    break;
-
-                case 'm':
-                    seconds += time * 60;
-                    break;
-
-                case 'h':
-                    seconds += time * 360;
-                    break;
-            }
-
-            TimeManager.getInstance().addPlayer(DataManager.getInstance().getPlayer(event.getPlayer().getName()), seconds);
-        }
-
-        if(UpdaterManager.getInstance().isUpdateAvailable() && event.getPlayer().hasPermission("prisonrankup.update")) {
-            UpdaterManager um = UpdaterManager.getInstance();
-
-            event.getPlayer().sendMessage(ChatColor.GOLD + "An update is available: " + um.getName() + ", a " +
-                    um.getType() + " for " + um.getVersion() + " available at " + um.getVersion());
         }
     }
 }
